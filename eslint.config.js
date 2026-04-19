6865bb17aea787b62639ea5eac4e9b4b9881e7f1
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-18 22:50:23 -0400

    feat: add ESLint configuration for code linting and formatting

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslintConfig from 'eslint-config-universal-code';
+
+const config = eslintConfig({
+  json: true,
+  stylistic: true,
+  unicorn: true,
+  yml: true,
+  perfectionist: true,
+  typescript: true,
+  ignore: ['postcss.config.cjs']
+});
+
+export default config;
